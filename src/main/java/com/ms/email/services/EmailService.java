35682c934c7037c7143443b21592962fa93ce250
Davi Marinho <davimarinho7742@gmail.com>
2024-02-12 07:59:15 -0300

    feat: create EmailService

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,47 @@
+package com.ms.email.services;
+
+import com.ms.email.domain.Email;
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+    @Autowired
+    private EmailRepository repository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Value(value = "${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public Email sendEmail(Email email){
+        try {
+            email.setSendDateEmail(LocalDateTime.now());
+            email.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+            emailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            email.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return repository.save(email);
+        }
+    }
+}
+
